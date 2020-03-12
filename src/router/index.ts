f7d6a2d56135d3bae8cb3e30be2201a165185cb2
s0lvang <august.s.solvang@gmail.com>
2020-03-12 22:26:46 +0100

    temp commit because we need to yeet ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,27 +1,48 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import firebase from 'firebase';
+import About from '../views/About.vue';
+import Login from '../views/Login.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home,
+    path: '*',
+    redirect: '/',
   },
   {
-    path: '/about',
+    path: '/login',
+    name: 'Login',
+    component: Login,
+  },
+
+  {
+    path: '/',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component: About,
+    meta: {
+      requiresAuth: true,
+    },
   },
 ];
 
 const router = new VueRouter({
   routes,
+  mode: 'history',
+});
+
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some((x) => x.meta.requiresAuth);
+  const { currentUser } = firebase.auth();
+
+  if (requiresAuth && !currentUser) {
+    next('/login');
+  } else if (requiresAuth && currentUser) {
+    next();
+  } else {
+    next();
+  }
 });
 
 export default router;
