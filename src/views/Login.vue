8a3583c7e6201c21233901007e111749cf0681ec
adrianht <adrianhthompson@gmail.com>
2020-03-15 22:05:17 +0100

    update loginpage

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,8 +1,28 @@
 <template>
   <div class="container mx-auto">
     <div class="flex flex-col items-center">
-      <p class="text-center">Logg inn med Google</p>
-      <a @click="socialLogin" class="align-middle">
+      <div >
+        <h1 class="text-4xl m-3 text-center">Karantenehjelpen</h1>
+        <p class="font-bold text-lg m-3">Hva er Karantenehjelpen?</p>
+        <p class="m-3">Når du sitter i hjemmekarantene skal du ikke oppholde
+          deg på steder der man lett kommer nær andre.
+          Da kan det bli veldig vanskelig å få handlet inn.
+          <br>
+          <br>
+          Karantenehjelpen skal hjelpe de som sitter i karantene
+          komme i kontakt med friske folk som ønsker å hjelpe.
+          <br>
+          <br>
+          Vi trenger både folk som er friske og vil stille opp for sine naboer,
+          og tilbyr handlehjelp til de som trenger det mest.
+        </p>
+        <h3 class="font-bold text-lg m-3">Følg FHI sine råd!</h3>
+        <p class="m-3">
+          Just do it
+        </p>
+      </div>
+      <p class="text-center mt-20 font-bold">Logg inn med Google</p>
+      <a @click="socialLogin" class="align-middle cursor-pointer">
         <img alt="Google Logo" src="../assets/google-logo.png" class="h-24">
       </a>
     </div>
