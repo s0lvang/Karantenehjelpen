93bd3db214fb99db26f25829550e01f7fb423114
adrianht <adrianhthompson@gmail.com>
2020-03-15 11:24:07 +0100

    add payment solution

--- src/components/RequestSummary.vue
+++ src/components/RequestSummary.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container mx-auto ">
-    <h2 class="text-3xl"> Din Bestilling </h2>
+    <h2 class="text-3xl text-center"> Din Bestilling </h2>
     <DetailedRequest :request="getRequest" />
     <Button
       btnText="Send Forespørsel"
@@ -38,6 +38,7 @@ export default {
         address: this.$store.getters.address,
         arrivalDescription: this.$store.getters.arrivalDescription,
         items: this.$store.getters.items,
+        paymentSolution: this.$store.getters.paymentSolution,
       };
     },
   },
