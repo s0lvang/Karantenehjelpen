025d32cd9455ff9dc74a0e111feaf601396860d0
plusk <andelar96@gmail.com>
2020-03-16 17:37:17 +0100

    Remove tailwind

--- postcss.config.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-/* eslint-disable global-require */
-/* eslint-disable no-tabs */
-module.exports = {
-  plugins: [require("tailwindcss")("tailwind.js"), require("autoprefixer")()]
-};
