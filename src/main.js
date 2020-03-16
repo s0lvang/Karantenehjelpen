498823aef1af8a24bbfb40bcde5040bb71836c86
plusk <andelar96@gmail.com>
2020-03-16 21:16:15 +0100

    Added reset-css

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import fb from "./firebaseConfig";
+import "@/assets/reset.css";
 
 Vue.config.productionTip = false;
 
