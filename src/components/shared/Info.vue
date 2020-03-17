276c1b274f0b505dec734b353522e7a07862fb78
plusk <andelar96@gmail.com>
2020-03-18 00:14:26 +0100

    Added two missing files

---
+++ src/components/shared/Info.vue
@@ -0,0 +1,21 @@
+<template>
+  <p><icon name="info" /> <slot></slot></p>
+</template>
+
+<script>
+import Icon from "@/components/shared/Icon.vue";
+
+export default {
+  name: "Info",
+  components: {
+    Icon
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+p {
+  text-align: center;
+  color: $color-primary;
+}
+</style>
