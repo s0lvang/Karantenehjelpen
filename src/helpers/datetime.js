fc407d54d0ce2371273716d127df78b4083991e1
Fredrik Malmo <fredrik.malmo@icloud.com>
2020-03-19 16:59:33 +0100

    Bruk moment for å formattere tid

--- src/helpers/datetime.js
+++ src/helpers/datetime.js
@@ -1,14 +1,28 @@
-const LOCALE = "nb-NO";
+import moment from "moment";
+
+const LOCALE = "nb";
+
+moment.locale(LOCALE);
+
+moment.updateLocale(LOCALE, {
+  relativeTime: {
+    future: "om %s",
+    past: "%s siden",
+    s: "et par sekunder",
+    ss: "%d sekunder",
+    m: "ett minutt",
+    mm: "%d minutter",
+    h: "en time",
+    hh: "%d timer",
+    d: "en dag",
+    dd: "%d dager",
+    M: "en måned",
+    MM: "%d måneder",
+    y: "et år",
+    yy: "%d år"
+  }
+});
 
 export default function formatDateTime(date) {
-  const options = {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-    hour: "numeric",
-    minute: "numeric"
-  };
-  const formatter = new Intl.DateTimeFormat(LOCALE, options);
-  return formatter.format(date);
+  return moment(date).fromNow();
 }
