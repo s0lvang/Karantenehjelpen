3646005f105906db68b3f380b70835469cf3ddc2
Fredrik Malmo <fredrik.malmo@icloud.com>
2020-03-19 17:34:59 +0100

    Bruk date-fns i stedet for moment

--- src/helpers/datetime.js
+++ src/helpers/datetime.js
@@ -1,28 +1,9 @@
-import moment from "moment";
-
-const LOCALE = "nb";
-
-moment.locale(LOCALE);
-
-moment.updateLocale(LOCALE, {
-  relativeTime: {
-    future: "om %s",
-    past: "%s siden",
-    s: "et par sekunder",
-    ss: "%d sekunder",
-    m: "ett minutt",
-    mm: "%d minutter",
-    h: "en time",
-    hh: "%d timer",
-    d: "en dag",
-    dd: "%d dager",
-    M: "en måned",
-    MM: "%d måneder",
-    y: "et år",
-    yy: "%d år"
-  }
-});
+import { formatDistanceToNow } from "date-fns";
+import { nb } from "date-fns/locale";
 
 export default function formatDateTime(date) {
-  return moment(date).fromNow();
+  return `${formatDistanceToNow(date, { locale: nb }).replace(
+    "omtrent",
+    ""
+  )} siden`;
 }
