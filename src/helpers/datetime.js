8f8929a293aa7fe064dd7d9ca02adf855f046cb7
oleast <oleast@stud.ntnu.no>
2020-03-20 00:18:26 +0100

    Import spedific date-fns modules to save on bundle size

--- src/helpers/datetime.js
+++ src/helpers/datetime.js
@@ -1,5 +1,5 @@
-import { formatDistanceToNow } from "date-fns";
-import { nb } from "date-fns/locale";
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
+import nb from "date-fns/locale/nb";
 
 export default function formatDateTime(date) {
   return `${formatDistanceToNow(date, { locale: nb }).replace(
