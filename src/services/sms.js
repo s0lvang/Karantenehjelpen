23e2f95dfe2b73961432a0831184890c809fc119
Fredrik Malmo <fredrik.malmo@icloud.com>
2020-03-21 12:31:05 +0100

    Bruk async/await i SMS-logikk

--- src/services/sms.js
+++ src/services/sms.js
@@ -2,20 +2,18 @@
 const twilioAccountSid = process.env.VUE_APP_ACCOUNT_SID;
 const twilioAuthToken = process.env.VUE_APP_AUTH_TOKEN;
 
-const encodeFormData = data => {
-  return Object.keys(data)
+const encodeFormData = data => (
+  Object.keys(data)
     .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
-    .join("&");
-};
+    .join("&")
+);
 
-export default (to, body) => {
-  fetch(
+export default async (to, body) => {
+  const resp = await fetch(
     `https://api.twilio.com/2010-04-01/Accounts/${twilioAccountSid}/Messages.json`,
     {
       method: "POST",
       body: encodeFormData({
-        // It might seem strange to not invert the next statement, but
-        // this is async so nothing should be logical. Accept it.
         Body: body,
         From: "Karanthjelp",
         To: `+47${to}`
@@ -28,7 +26,7 @@ export default (to, body) => {
       }
     }
   )
-    .then(resp => resp.json())
-    .then(data => console.info(`SMS sent ${data.date_created} to ${data.to}.`))
-    .catch(err => console.warn(`Could not send SMS. ${err}.`));
+
+  const data = await resp.json();
+  console.info(`SMS sent ${data.date_created} to ${data.to}.`);
 };
